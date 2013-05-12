943ec1c1ceedd8d7cd105658cd378b90a84aab42
Sean Riggle <seanriggle@gmail.com>
2013-05-12 14:32:32 -0400

    refactoring views

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('HistoryCtrl', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('TagCtrl', [function() {
 
   }]);
