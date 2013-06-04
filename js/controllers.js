531df9a2fc44a70493084aa725a2e50e334a22be
Sean Riggle <seanriggle@gmail.com>
2013-06-03 23:37:10 -0400

    adding updates

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,38 @@
 function TagCtrl($scope) {
     $scope.status = "Tag a pothole!";
+    
+    $scope.imgSource = "img/pothole_large.png"; // source for img tag to display picture
+    $scope.pictureSource;   // picture source
+    $scope.destinationType; // sets the format of returned value 
+
+    // Wait for Cordova to connect with the device
+    //
+    document.addEventListener("deviceready",onDeviceReady,false);
+
+    // Cordova is ready to be used!
+    //
+    function onDeviceReady() {
+        pictureSource= Camera.PictureSourceType.CAMERA;
+        destinationType= Camera.DestinationType.FILE_URI;
+        alert("onDeviceReady");
+    }
+    
+    $scope.takePicture = function(){
+        alert("take pic");
+        var options = {
+            sourceType: $scope.pictureSource,
+            destinationType: $scope.destinationType
+        };
+        navigator.camera.getPicture($scope.onCameraSuccess, $scope.onError, options);
+    }
+    
+    $scope.onCameraSuccess = function(imageURI){
+        $scope.imgSource = imageURI;
+    }
+    
+    $scope.onError = function(message) {
+        alert(message);
+    }        
 }
 
 function HistoryCtrl($scope) {
